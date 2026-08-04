10a8a052d0bfc151087b6587754255c44a8ee1fa
Nicolai Palmhert <103109153+nicolaipalmhert@users.noreply.github.com>
2026-08-04 12:30:51 +0000

    [LeetCode Sync] Runtime - 6 ms (21.59%), Memory - 46 MB (33.90%)

---
+++ solutions/0009-palindrome-number/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        String number = String.valueOf(x);
+
+        int left = 0;
+        int right = number.length() - 1;
+
+        while (left < right) {
+            if (number.charAt(left) == number.charAt(right)) {
+                left++;
+                right--;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
